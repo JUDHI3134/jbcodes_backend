5bf006f2e31839c33d325e2cdd4a63eefd213455
umesh604@gmail.com <umesh604@gmail.com>
2024-09-15 14:07:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+require("dotenv").config()
+const app = express();
+
+const port = 8000;
+
+app.get("/",(req,res)=>{
+    res.send("Server run Successfully")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("This is our about page")
+})
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server running port number ${port}`);
+    
+})
